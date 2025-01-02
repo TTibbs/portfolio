1f50989853b0b1c736a140636e2d94755a2f266b
TTibbs <terryjw.97@gmail.com>
2025-01-02 19:04:45 +0000

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,39 @@ import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Layout from "./components/Layout";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <Home />
-          </Layout>
-        }
-      />
-      <Route
-        path="/projects"
-        element={
-          <Layout>
-            <Projects />
-          </Layout>
-        }
-      />
-      <Route
-        path="/contact"
-        element={
-          <Layout>
-            <Contact />
-          </Layout>
-        }
-      />
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/projects"
+          element={
+            <Layout>
+              <Projects />
+            </Layout>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <Layout>
+              <Contact />
+            </Layout>
+          }
+        />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
